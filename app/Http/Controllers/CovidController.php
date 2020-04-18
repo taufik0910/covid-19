aaab5a31fef47ffaa49a4ec5bde0c80d700c4c0c
taufik0910 <taufik.cradle@gmail.com>
2020-04-19 02:36:16 +0700

    chartjs dengan api corona

--- app/Http/Controllers/CovidController.php
+++ app/Http/Controllers/CovidController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Charts\CovidChart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CovidController extends Controller
 {
     //
+public function chart()
+{
+    # code...
+    $suspect =collect (http::get('https://api.kawalcorona.com/indonesia/provinsi')->json());
+    //dd($suspect->flatten(1));
+$suspectData =$suspect->flatten(1);
+    $labels = $suspectData->pluck('Provinsi');
+    $data = $suspectData->pluck('Kasus_Posi');
+    $death = $suspectData->pluck('Kasus_Meni');
+    $colors = $labels->map(function($item){
+        return '#' . substr(md5(mt_rand()),0 ,6 );
+    });
+
+    $chart = new CovidChart;
+    $chart ->labels($labels);
+    $chart->dataset('data Kasus Positif Corona Di Indonesia','pie',$data, $death)->backgroundColor($colors);
+    
+
+    return view('coron',[
+                'chart'=>$chart,
+                ]);
+
+}
 }
---
+++ resources/views/coron.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Covid-19 Indonesia</title>
+</head>
+<body><div>
+    <h1>Sebaran Kasus Corona di Indonesia</h1>
+    {!! $chart->container() !!}
+    {!! $chart->script() !!}
+
+    
+</div>
+
+    
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+
+
+</body>
+</html>
